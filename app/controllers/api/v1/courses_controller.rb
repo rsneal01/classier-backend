2b3795f3f24566337605898f9ee26545d8ca133e
rsneal01 <rsneal01@gmail.com>
2022-03-05 09:34:46 -0800

    modified courses and teachers controllers, modified routes

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,2 +1,40 @@
 class Api::V1::CoursesController < ApplicationController
+
+    before_action :set_teacher
+
+    def index
+        @courses = Course.all
+        render json: @courses
+    end
+
+    
+
+    def show
+        @course = Course.find(params[:id])
+    end
+
+    def create
+        @course = Course.create(course_params)
+        if @course.save
+            render json: @course
+        else
+            render json: {error: 'Error creating course'}
+        end
+    end
+
+    def destroy
+        @course = Course.find(params[:id])
+        @course.destroy
+    end
+
+    private
+
+    def set_teacher
+        @teacher = Teacher.find(params[:teacher_id])
+    end
+
+    def course_params
+        params.require(:course).permit(:title, :teacher_id, :body, :description)
+    end
+
 end
--- app/controllers/api/v1/teachers_controller.rb
+++ app/controllers/api/v1/teachers_controller.rb
@@ -1,2 +1,31 @@
 class Api::V1::TeachersController < ApplicationController
+
+    def index
+        @teachers = Teacher.all
+        render json: @teachers
+    end
+
+    def create
+        @teacher = Teacher.create(teacher_params)
+        if @teacher.save
+            render json: @teacher
+        else
+            render json: {error: 'Error creating teacher'}
+        end
+    end
+
+    def show
+        @teacher = Teacher.find(params[:id])
+    end
+
+    def destroy
+        @teacher = Teacher.find(params[:id])
+        @teacher.destroy
+    end
+
+    private
+
+    def teacher_params
+        params.require(:teacher).permit(:name)
+    end
 end
